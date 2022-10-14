a1f94cd32de51d3036562a98c57027eacf432ef8
Frankenstei <enwereloveday@yahoo.com>
2022-10-14 13:32:34 -0700

    2 numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - printing numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m = 0;
+	int n = 1;
+
+	while (m <= 8)
+	{
+		int n = 0;
+		while (n <= 9)
+		{
+			if (m != 0 && n > m)
+			{
+				putchar(m + '0');
+				putchar(n + '0');
+				if (m < 8 && n < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				n++;
+			}
+		}
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
