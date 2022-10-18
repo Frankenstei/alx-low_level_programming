4ebf8fc9a897007127fa508988bd466da957c72e
Frankenstei <enwereloveday@yahoo.com>
2022-10-18 14:40:45 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - prints alphabets
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int n = 0;
+
+	while (n <= 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
