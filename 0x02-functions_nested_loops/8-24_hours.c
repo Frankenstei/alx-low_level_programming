d036d115718ef66fc52de96c823a6f77cf2a128a
Frankenstei <enwereloveday@yahoo.com>
2022-10-18 16:59:34 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int z = 0;
+
+	while (z <= 23)
+	{
+		int y = 0;
+		while (y <= 59) 
+		{
+			int b = 0;
+			while (b <= 60)
+			{
+				int a = 0;
+				while (a <= 60)
+				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(y + '0');
+					_putchar(z + '0');
+					a++;
+				}
+				_putchar('\n');
+				b++;
+			}
+			y++;
+		}
+		z++;
+	}
+}
