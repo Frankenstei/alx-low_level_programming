c4944274f5a04fe9b8c5fc61487537ea8f664e8e
Frankenstei <enwereloveday@yahoo.com>
2022-10-18 15:47:23 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * _islower - checks for lowercase letter, 1 if lowercase
- * 0 otherwise
+ * _islower - returns 1 for lowercase 0 otherwise
  *
  * Returns: 1 for lowercase and 0 otherwise
  *
