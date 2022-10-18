58b93b6ad256deff3d2520289b0cf4b12bcdc18b
Frankenstei <enwereloveday@yahoo.com>
2022-10-18 14:20:33 -0700

    print_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,22 @@
-#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
 
 
