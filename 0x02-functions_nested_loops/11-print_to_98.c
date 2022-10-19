b0c1493f728f2d37d27b49e32be345a0b9093bef
Frankenstei <enwereloveday@yahoo.com>
2022-10-18 18:28:34 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**print_to_98  - prints to nighty eight
+ *
+ * Return: Always 0
+ *
+ * @n: is an integer
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	printf('\n');
+}
