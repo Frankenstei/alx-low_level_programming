4e7022a4b346336d9f2edd04a6461eb66ff51f70
Frankenstei <enwereloveday@yahoo.com>
2022-10-18 20:18:50 -0700

    times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * print_times_table - prints times table
+ *
+ * Return: Always 0
+ *
+ * @n: integer
+ */
+
+void print_times_table(int n)
+{
+	int a, b, c;
+	if (n >= 0 && n <= 15)
+	{
+		a = 0;
+
+		while (a <= n)
+		{
+			b = 0;
+
+			while (b <= n)
+			{
+				if (b == 0)
+				{
+					_putchar((a * b) + '0');
+				}
+				else if ((a * b) < 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((a * b) + '0');
+				}
+				else if ((a * b) >= 10 && (a * b) < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((a * b) / 10) + '0');
+					_putchar(((a * b) % 10) + '0');
+				}
+				else if ((a * b) >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(((a * b) / 10) + '0');
+					_putchar((((a * b) / 10) % 10) + '0');
+					_putchar(((a * b) % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
