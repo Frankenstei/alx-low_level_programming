ce5ed1e29e217960363280a3f61459bc4eede8b3
Frankenstei <enwereloveday@yahoo.com>
2022-10-20 15:05:12 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,26 +10,21 @@
 
 void print_square(int size)
 {
-	int i = 0;
+	int i, j;
 
-	while (i < size)
+	if (size <= 0)
 	{
-		if (size <= 0)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
 		{
-			_putchar('\n');
-		}
-		else
-		{
-			int j = 0;
-
-			while (j < size)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
-				j++;
 			}
 			_putchar('\n');
 		}
-		i++;
 	}
-	_putchar('\n');
 }
