33ec276613caaeeca92e51481e455e2ba6c63da4
Frankenstei <enwereloveday@yahoo.com>
2022-10-20 11:50:08 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ *
+ * @size: integer
+ *
+ * Return: Always 0
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	while (i < size)
+	{
+		int j = 0;
+
+		while (j < size)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++
+	}
+	_putchar('\n');
+}
