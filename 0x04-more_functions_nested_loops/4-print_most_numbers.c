3f64f0785c00965dcdabdb4e022ec08a4746a05b
Frankenstei <enwereloveday@yahoo.com>
2022-10-20 09:13:55 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints numbers except two and four
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
