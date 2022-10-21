ec2cfbd25360e3cbbed916d535d920599582068d
Frankenstei <enwereloveday@yahoo.com>
2022-10-20 18:17:01 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,13 +16,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (int i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for(int j = 1; j <= (size - i); j++)
+			for (j = 1; j < (size - i); j++)
 			{
 				_putchar(' ');
 			}
-			for (int k = 1; k <= i; k++)
+			for (int k = 0; k <= i; k++)
 			{
 				_putchar('#');
 			}
