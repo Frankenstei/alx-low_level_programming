677467e9ba90a22a671a2233cfeebe64ba82fde0
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 16:05:43 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ *
+ * @str: character string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (; *str != '\0'; s = s+2;)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
