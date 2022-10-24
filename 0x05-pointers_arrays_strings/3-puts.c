65c720ad6e08dccb23662975a9ef44f619d26c75
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 06:57:53 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - writes str to stdout
+ *
+ * @str: a character
+ *
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	while (s != '\0')
+	{
+		_putchar(s);
+		s++;
+	}
+	_putchar('\n');
+	return (0);
+}
