02b40f0597cfe017f816f677b8a916d9c6570d10
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 07:50:34 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** print_rev - reverse the order of a string
+ *
+ * @s: character string
+ *
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	for (s[_strlen(s) - 1]; *s != '\0'; s--)
+	{
+		if (_strlen(s) - 1 > 0)
+		{
+			_putchar(s);
+		}
+	}
+	_putchar('\n');
+}
