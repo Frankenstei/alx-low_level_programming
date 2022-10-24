00495458449752da6436a4e386f6b62046fe9fa9
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 06:20:38 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * Return: an integer
+ *
+ * @s: string of type char
+ */
+
+int _strlen(char *s)
+{
+	int num = 0;
+
+	while (*s != '\0')
+	{
+		num++;
+	}
+	return num;
+}
