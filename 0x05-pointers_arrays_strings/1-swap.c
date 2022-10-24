0bc9a4df1c61f6da95c23c3cf58b15fe00f2b8aa
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 06:47:04 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int store;
+	store = *a;
+	*a = *b;
+	*b = store;
+}
