5b5db38c0a8a4bdd1cc400399a7eab69e27e0a54
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 17:00:49 -0700

    put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: character string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	while (*str != '\0')
+	{
+		length++;
+	}
+
+	length = length -1;
+
+	for (i = length / 2; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
