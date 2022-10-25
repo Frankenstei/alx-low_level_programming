a93761f37f0288de1f9d9d59a46f899e05bc97a8
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 17:52:29 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies src to dest
+ *
+ * @dest: character string
+ * @src: character string
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (dest[i] = src[i])
+	{
+		i++;
+	}
+	return (dest[i]);
+}
