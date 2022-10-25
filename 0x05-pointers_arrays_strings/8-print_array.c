6d041c3dfcf3ee4f630a21d4d66c3f48a65324d7
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 17:23:18 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints arrays
+ *
+ * @a: array name
+ *
+ * @n: array number
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	for (i = 0; i == n - 1; i++)
+	{
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
