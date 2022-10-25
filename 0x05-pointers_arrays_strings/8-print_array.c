9da05e4a437d91e4c57ba359dbcd3825610cd265
Frankenstei <enwereloveday@yahoo.com>
2022-10-24 17:28:59 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,11 @@
 
 void print_array(int *a, int n)
 {
-	for (int i = 0; i == n - 1; i++)
+	int i;
+
+	for (i = 0; i == n - 1; i++)
 	{
-		printf("%d", &a[i]);
+		printf("%d", a[i]);
 	}
 	printf("\n");
 }
