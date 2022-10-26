5c4abd09c86c799876bd7592612e208cecbf677f
Frankenstei <enwereloveday@yahoo.com>
2022-10-25 23:33:23 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: destination pointer
+ * @src: source pointer
+ *
+ * Return: a pointer dest
+ */
+
+*char *_strcat(char *dest, char *src)
+{
+
