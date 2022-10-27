bc66e82e268d6b7c03d8d117670652e579b1bb03
Frankenstei <enwereloveday@yahoo.com>
2022-10-27 04:03:40 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,35 @@
 #include "main.h"
+
+/**
+ * cap_string - capitalize
+ *
+ * @s: string
+ *
+ * Return: pointer
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		for (i == 0; *(s + i); i++)
+		{
+			if (i == 0)
+			{
+				*(s + i) = s[i] - 32;
+			}
+
+			if (s[i] == ',' || s[i] == ';' s[i] == '.' || s[i] == '|'
+					|| s[i] == '?' || s[i] == '"'
+					|| s[i] == '(' || s[i] == ')'
+					|| s[i] == '{' || s[i] == '}')
+
+			{
+				*(s + i) = s[i + 2] - 32;
+			}
+		}
+	}
+	return (s);
+}
