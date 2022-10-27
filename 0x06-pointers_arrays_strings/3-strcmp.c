f7c6716e2b203bafa5ff38d97ccdd0875a0f5859
Frankenstei <enwereloveday@yahoo.com>
2022-10-27 03:17:48 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,27 @@
 #include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ *
+ * @s1: string character
+ * @s2: string character
+ *
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else if (*s1 > *s2)
+	{
+		return ((*s1 - *s2));
+	}
+	else
+	{
+		return ((*s1 = *s2));
+	}
+}
