12fc5d14c7faaba7ef03deb7a463ed1fea2b590e
Frankenstei <enwereloveday@yahoo.com>
2022-10-27 15:42:37 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ *
+ * @n: integer
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	i = n;
+	
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar(x % 10 + '0');
+}
