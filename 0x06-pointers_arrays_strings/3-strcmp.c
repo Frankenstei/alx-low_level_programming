43a5f34771a57e5cf961a35c95c68bafb44bd01b
Frankenstei <enwereloveday@yahoo.com>
2022-10-27 14:00:06 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,17 +11,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
+	int i, j;
+
+	for (i = 0, j = 0; (*(s1 + i) != '\0' && *(s2 + j) != '\0') && (*(s1 + i) == *(s2 + j)); i++, j++)
+	{
+		;
+	}
+	if (*(s1 + i) == *(s2 + j))
 	{
 		return (0);
 	}
-
-	else if (*s1 > *s2)
-	{
-		return ((*s1 - *s2));
-	}
 	else
 	{
-		return ((*s1 - *s2));
-	}
+		return ((*(s1 + i)) - (*(s2 + j)));
 }
