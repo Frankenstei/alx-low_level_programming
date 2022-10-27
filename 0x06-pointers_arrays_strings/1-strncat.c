38cd795d4ab7d42d0a83ca778f204789a2944d95
Frankenstei <enwereloveday@yahoo.com>
2022-10-27 06:11:53 -0700

    concatenate n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest++;
 	}
 
-	for (i = 0; *(src + i) == *(src + n); i++, lenght++)
+	for (i = 0; *(src + i) == *(src + n); i++, length++)
 	{
 		*(dest + length) = *(src + i);
 	}
