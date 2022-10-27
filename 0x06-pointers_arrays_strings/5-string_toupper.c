38191450119b0949f5c0866d6074620f841a7491
Frankenstei <enwereloveday@yahoo.com>
2022-10-27 07:47:00 -0700

    concatenate n

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,9 +14,9 @@ char *str_toupper(char *str)
 
 	for (i = 0; *(str + i) != '\0'; i++)
 	{
-		if (*(str + i) >= 97 && *(str + i) <= 122)
+		if ((*(str + i) >= 97) && (*(str + i) <= 122))
 		{
-			*(str + i) = *(str + i) - 32;
+			*(str + i) = (*(str + i)) - 32;
 		}
 	}
 	return (str);
