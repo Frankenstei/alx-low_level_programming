44ebff155e8b6f5beecf5af3c99ff10b5bc73b2c
Frankenstei <enwereloveday@yahoo.com>
2022-10-27 03:32:45 -0700

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * str_toupper - coverts strings to uppercase
+ *
+ * @str: string to be converted
+ *
+ * Return: pointer
+ */
+
+char *str_toupper(char *str)
+{
+	int i;
+
+	while (*str != '\0')
+	{
+		for (i = 0; *(str + i); i++)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				*(str + i) = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
