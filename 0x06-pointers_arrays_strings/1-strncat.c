4afa688c68419332079d7a055b916163ccef0438
Frankenstei <enwereloveday@yahoo.com>
2022-10-27 03:12:43 -0700

    copy n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+ * _strncat - concatenate n bytes from nother string
+ *
+ * @n: integer n
+ * @dest: destination character
+ * @src: source character
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, length = 0;
+
+	while (*dest != '\0')
+	{
+		length++;
+		dest++;
+	}
+
+	for (i = 0; *(src + i) == *(src + n); i++, lenght++)
+	{
+		*(dest + length) = *(src + i);
+	}
+
+	*(dest + length) = '\0';
+
+	return  (dest);
+}
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * _strncpy - copies n bytes to another string
+ *
+ * @n: integer
+ * @dest: destination character
+ * @src: source character
+ *
+ * Return: pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] == src[n]; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
