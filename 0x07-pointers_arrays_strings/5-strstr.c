f9f170b52eee57a52003229eac2df1405aec5027
Frankenstei <enwereloveday@yahoo.com>
2022-10-31 16:06:45 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: main character string
+ * @needle: substring
+ *
+ * Return: s or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned i, j;
+
+	for (i = 0; *(needle + i) != '\0'; i++)
+	{
+		for (j = 0; *(haystack + j) != '\0'; j++)
+		{
+			if (*(haystack + j) == *(needle + i))
+			{
+
+
