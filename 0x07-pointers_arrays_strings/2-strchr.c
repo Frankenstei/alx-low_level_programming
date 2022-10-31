8e8247fdfab292d2f269f292304c8e5b8d2820ea
Frankenstei <enwereloveday@yahoo.com>
2022-10-31 11:27:25 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - returns a pointer to c in s or null
+ *
+ * @s: character string
+ * @c: character c
+ *
+ * Return: pointer or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) == '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s);
+			break;
+		}
+	}
+	return (NULL);
+}
