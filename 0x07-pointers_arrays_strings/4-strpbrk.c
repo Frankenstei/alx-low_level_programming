277803fc567fa2a97942e1b83700987b06c18826
Frankenstei <enwereloveday@yahoo.com>
2022-10-31 15:07:12 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches for any of a set of bytes
+ *
+ * @s: character string
+ * @accept: charater string
+ *
+ * Return: pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(accept + i) != '\0'; i++)
+	{
+		for (j = 0; *(s + j) != 32; j++)
+		{
+			if (*(s +  j) == *(accept + j))
+			{
+				return ((s + i));
+			}
+		}
+	}
+
+	return ('\0');
+}
