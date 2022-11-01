c5ec6cb17dab331f8355d495873fddd19f632d98
Frankenstei <enwereloveday@yahoo.com>
2022-10-31 18:06:37 -0700

    diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,17 +10,14 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 
 	int diag1 = 0, diag2 = 0;
 
-	for (i = 0, j = 0; i < size; i++, j++)
+	for (i = 0; i < size; i++)
 	{
-		diag1 += a[i][j];
-	}
-	for (i = 0, j = (size - 1); i < size; i++, j--)
-	{
-		diag2 += a[i][j];
+		diag1 = diag1 + a[(size + 1) * i)];
+		diag2 = diag2 + a[(size - 1) * (i + 1)];
 	}
 	printf("%d, %d", diag1, diag2);
 }
