4b51837af504eef796c2e32877aa933bb5bb7d10
Frankenstei <enwereloveday@yahoo.com>
2022-11-02 04:51:06 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: string character
+ *
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > 0)
+	{
+		_putchar(*(s + ((sizeof(*s) /sizeof(char)) - 1)));
+		_print_rev_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
