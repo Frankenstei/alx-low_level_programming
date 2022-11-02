d5407aace0f3c5d4337fd32f4e86826722cb52dd
Frankenstei <enwereloveday@yahoo.com>
2022-11-02 04:22:06 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,9 +12,11 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return;
+		_putchar(*s);
+		_puts_recursions(s + 1);
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(*s);
+	else
+	{
+		_putchar('\n');
+	}
 }
