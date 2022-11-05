94123de103506a91ba54988bf6201019855aa2dc
Frankenstei <enwereloveday@yahoo.com>
2022-11-04 17:24:33 -0700

    sl

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - lower
+ *
+ * @c: integer
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - is alpha
+ *
+ * @c: int
+ *
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+}
