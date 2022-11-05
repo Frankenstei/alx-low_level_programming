d4b90e9af4cff535cdc43bb4a7ea60539d12bd8d
Frankenstei <enwereloveday@yahoo.com>
2022-11-04 19:07:53 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,4 +12,42 @@
 
 int main(argc, *argv[])
 {
+	int cents = atoi(argv[1]), i;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else
+	{
+		while (cents > 0)
+		{
+			if (cents >= 25)
+			{
+				cents = cents - 25;
+			}
+			else if (cents >= 10)
+			{
+				cents = cents - 10;
+			}
+			else if (cents >= 5)
+			{
+				cents = cents - 6;
+			}
+			else if (cents >= 2)
+			{
+				cents = cents - 3;
+			}
+			else if (cents >= 1)
+			{
+				cents = cents - 1;
+			}
+			i++;
+		}
+		printf("%i\n", i);
+		return (0);
+	}
+}
 
