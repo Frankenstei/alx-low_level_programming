ca701a0ca0382fde6b99792303fa7d6e3026bb86
Frankenstei <enwereloveday@yahoo.com>
2022-11-04 18:13:06 -0700

    sl

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints minimum number of coin change
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0, 1
+ */
+
+int main(argc, *argv[])
+{
+
