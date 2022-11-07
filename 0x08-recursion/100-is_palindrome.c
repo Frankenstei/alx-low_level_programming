82b553dc6eae27da695daf23d2f8a7e4f5a9c315
Frankenstei <enwereloveday@yahoo.com>
2022-11-07 10:18:25 -0800

    recursiom

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * is_palindrome - returns 1 if palidrome 0 otherwise
+ *
+ * @s: character string
+ *
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s =!
