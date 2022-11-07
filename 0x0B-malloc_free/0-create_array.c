dad7cbf593338a4146a531673a19b1b879a69886
Frankenstei <enwereloveday@yahoo.com>
2022-11-07 10:19:34 -0800

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,17 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	else
+	char *ptr;
+
+	ptr = malloc(size * sizeof(char));
+
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
 	{
-		
+		ptr[i] = c;
+	}
+
+	return ptr;
+
 }
