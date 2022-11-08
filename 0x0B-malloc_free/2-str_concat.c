e24f89ed270c69b9e282d63dc4ca9ee078fecc54
Frankenstei <enwereloveday@yahoo.com>
2022-11-08 05:06:48 -0800

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -36,7 +36,7 @@ char *str_concat(char *s1, char *s2)
 
 	s1[i] = '\0';
 
-	ptr = (char *)malloc((i + 1) * sizeof(char));
+	ptr = (char *)malloc((i + j + 1) * sizeof(char));
 	if (ptr == NULL)
 	{
 		return (NULL);
