6c1e5aec0cde1a6c3b2e3a28a9a7be3df10ca895
Frankenstei <enwereloveday@yahoo.com>
2022-11-10 14:30:12 -0800

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, length  length1, length2;
 	char *ptr;
 
-	if (s2 == NULL)
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 = NULL)
 	{
 		s2 = "";
 	}
