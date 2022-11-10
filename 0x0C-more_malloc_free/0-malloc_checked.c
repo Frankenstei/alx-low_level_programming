5417f047dcba3ae1c18f75a0a98034609b44371e
Frankenstei <enwereloveday@yahoo.com>
2022-11-10 11:26:30 -0800

    address return

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - returns
+ *
+ * @b: integer
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr =malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr)
+}
