10ff6b7fdf802e2bdc942882f93c5770b99eb161
Frankenstei <enwereloveday@yahoo.com>
2022-11-10 17:48:46 -0800

    strnconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, length  length1, length2;
+	unsigned int i, length, length1, length2;
 	char *ptr;
 
 	if (s1 == NULL)
