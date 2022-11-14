611565652f3da993d1ab2d2122470293ffda11ea
Frankenstei <enwereloveday@yahoo.com>
2022-11-14 13:06:23 -0800

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,45 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#endif
+/**
+ * print_dog - print struct dog
+ *
+ * @d: struct dog
+ *
+ * Return: 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+
+		}
+		else
+		{
+			printf("Name: %s", d->name);
+		}
+		if (d->age == NULL)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %d\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %d\n", d->owner);
+		}
+	}
+}	
+
+
