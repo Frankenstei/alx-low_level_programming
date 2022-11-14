d830806cead5c21c14c541dd0ac5f95b7a4a8d96
Frankenstei <enwereloveday@yahoo.com>
2022-11-14 12:27:13 -0800

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,6 +5,7 @@
 /**
  * init_dog - initializes struct dog
  *
+ * @d: struct dog
  * @name: pointer
  * @age: float
  * @owner; pointer
@@ -13,7 +14,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	if (d != NULL)
+	{
+		d -> name = name;
+		d -> age = age;
+		d -> owner = owner;
+	}
 }
