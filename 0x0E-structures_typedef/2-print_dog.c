a7c44a010b8ff317d6a7b465e60dbbe4a1a39b94
Frankenstei <enwereloveday@yahoo.com>
2022-11-14 13:46:17 -0800

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -22,11 +22,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", d->name);
 		}
-		if (!(d->age))
-		{
-			printf("Age: (nil)\n");
-		}
-		else
+		if (d->age)
 		{
 			printf("Age: %f\n", d->age);
 		}
