94079cbddbe87458207dbc301e173101478e3b3e
Frankenstei <enwereloveday@yahoo.com>
2022-11-16 14:48:59 -0800

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function
+ *
+ * @array: pointer
+ * @size: array size
+ * @cmp: function pointer
+ *
+ * Return: -1 or index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && cmp)
+	{
+		int i;
+
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			if ((*cmp)(array[i]))
+			{
+				return (i);
+			}
+			else
+			{
+				return (-1);
+			}
+		}
+	}
+}
