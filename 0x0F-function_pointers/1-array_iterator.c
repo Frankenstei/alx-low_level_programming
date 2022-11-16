7f8fce113282b1e1b86a4fcfe7f11d340b14839d
Frankenstei <enwereloveday@yahoo.com>
2022-11-16 14:38:38 -0800

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (array && action)
+	if (array && action && size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
