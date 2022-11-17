3238a816935467417e707e3ff83f8623e8f8f598
Frankenstei <enwereloveday@yahoo.com>
2022-11-17 08:13:01 -0800

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -20,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		sum = sum + arg(ap, unsigned int);
+		sum = sum + va_arg(ap, unsigned int);
 	}
 	va_end(ap);
 	return (sum);
