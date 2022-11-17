e1ab7544e74a7ebe7a61495e5d07b37b752da85e
Frankenstei <enwereloveday@yahoo.com>
2022-11-16 17:07:25 -0800

    calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+ #include "3-calc.h"
+
+/**
+ * main - function main
+ * 
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	sign = get_op_func(argv[2]);
+	if (!(sign))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	printf("%d\n", op(num1, num2));
+	return (0);
+}
+
+
