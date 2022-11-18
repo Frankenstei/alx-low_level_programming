e1d995e530741924f2b9008cc70b642d8cb4fdc0
Frankenstei <enwereloveday@yahoo.com>
2022-11-17 19:36:01 -0800

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,8 +4,101 @@
  * print_all - print all argument
  *
  * @format: constant pointer constant parameter
+ *
+ * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
+	va_list ap;
+	form_t form[] = 
+	{
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_charx}
+	};
+	unsigned int i = 0, j;
+	char *separator = "";
 
+	va_start(ap, format);
+	while (format != NULL && form[i])
+	{
+		j = 0;
+
+		while (j < 4)
+		{
+			if (format[i] == *form[j].charType)
+			{
+				form[j].f(separator, ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
+
+/**
+ * print_char - prints char
+ * @separator: separator
+ * @ap: argument
+ *
+ * Return: void
+ */
+
+void print_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+ * print_int - print integer
+ *
+ * @separator: separator
+ * @ap: argument
+ *
+ * Return: void
+ */
+
+void print_int(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, int));
+}
+
+/**
+ * print_float - print float
+ *
+ * @separator: separator
+ * @ap: argument
+ *
+ * Return: void
+ */
+
+void print_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, int));
+}
+
+/**
+ * print_charx
+ *
+ * @separator: separator
+ * @ap: argument
+ *
+ * Return: void
+ */
+
+void print_charx(char *separator, va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (s == NULL)
+	{
+		printf("%s(null)", separator);
+		return;
+	}
+	printf("%s%s", separator, s);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(char *separator, va_list ap);
+void print_int(char *separator, va_list ap);
+void print_float(char *separator, va_list ap);
+void print_charx(char *separator, va_list ap);
+
+/**
+ * struct format - struct name
+ * @charType: specifier
+ * @f: function pointer
+ */
+
+typedef struct form
+{
+	char *charType;
+	void (*f)(char *separator, va_list ap);
+} form_t;
 
 #endif
