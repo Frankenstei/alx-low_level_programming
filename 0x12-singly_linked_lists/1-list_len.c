55a513135729c80f7885c0b7d14576aacd46ef9c
Frankenstei <enwereloveday@yahoo.com>
2022-11-24 08:25:18 -0800

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/** list_len - returns number of elements
+ *
+ * @h: linked list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elemNo = 0;
+
+	while (h != NULL)
+	{
+		h  = h->next;
+		elemNo++;
+	}
+	return (elemNo);
+}
