0f7c6fb80500de5a83242dc65dce0416d830f6b3
Frankenstei <enwereloveday@yahoo.com>
2022-11-24 12:42:33 -0800

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,20 +14,21 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *tempMem;
 	unsigned int i;
 
-	tempMem = malloc(sizeof(list_t));
-	if (tempMem == NULL)
+	if (head != NULL && str != NULL)
 	{
-		return (NULL);
+		tempMem = malloc(sizeof(list_t));
+		if (tempMem == NULL)
+		{
+			return (NULL);
+		}
+		tempMem->str = strdup(str);
+		for (i = 0; str != '\0'; i++)
+		{
+			;
+		}
+		tempMem->len = i;
+		tempMem->next = *head;
+		*head = tempMem;
+		return (tempMem);
 	}
-
-	tempMem->str = strdup(str);
-	for (i = 0; str != '\0'; i++)
-	{
-		;
-	}
-	tempMem->len = i;
-	tempMem->next = *head;
-	*head = tempMem;
-
-	return (tempMem);
 }
