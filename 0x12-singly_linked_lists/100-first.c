a81fb06076cfb20ee316e00b7d2e5e4dfee1bcdb
Frankenstei <enwereloveday@yahoo.com>
2022-11-24 18:47:26 -0800

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * execFirst - executes before main
+ *
+ * Return: nothing
+ */
+
+void execFirst() __attribute__((constructor));
+void execFirst()
+{
+	printf("You're beat! and yet, you must allow,\nI bore
+		       	my house upon my back!\n");
+}
