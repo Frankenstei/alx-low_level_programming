668432440a32336889f1c7019e0311d040831f6b
Frankenstei <enwereloveday@yahoo.com>
2022-11-24 18:54:09 -0800

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,7 @@
  * Return: nothing
  */
 
-void execFirst(void) __attribute__((constructor));
-void execFirst(void)
+void __attribute__((constructor)) execFirst(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
