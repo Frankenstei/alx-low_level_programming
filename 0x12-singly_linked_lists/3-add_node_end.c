c6699dc180b411b6870c437b219d589ef5824a1f
Frankenstei <enwereloveday@yahoo.com>
2022-11-24 18:33:47 -0800

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,8 +11,33 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t tempMem;
+	list_t tempMem, temp;
+	size_t i;
 
 	tempMem = malloc(sizeof(list_t));
 
-	if (head ==
+	if (tempMem == NULL)
+	{
+		return (NULL);
+	}
+	tempMem->str = strdup(str);
+	for (i = 0; str[i]; i++)
+	{
+		;
+	}
+	tempMem->len = i;
+	tempMem->next = NULL;
+	temp = *head;
+	if (temp == NULL)
+	{
+		*head = tempMem;
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = tempMem;
+	}
+	return (*head);
