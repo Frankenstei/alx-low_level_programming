a425870597586494d2b935aa91e94e70c3bbbd03
Frankenstei <enwereloveday@yahoo.com>
2022-11-29 14:20:51 -0800

    sum node

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -12,14 +12,15 @@ int sum_listint(listint_t *head)
 {
 	int sum = 0;
 	
-	if (head == NULL)
+	if (head)
 	{
-		return (0);
-	}
-	while(head ! = NULL)
-	{
-		sum = sum + head->n;
-		head = head->next;
+		while(head ! = NULL)
+		{
+			sum = sum + head->n;
+			head = head->next;
+		
+		
+		}
 	}
 	return (sum);
 }
