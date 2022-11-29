eaf60ca6bb621eeb633ed9dc391dc5ad68d9d2a4
Frankenstei <enwereloveday@yahoo.com>
2022-11-29 13:36:35 -0800

    pop head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@
 int pop_listint(listint_t **head)
 {
 	int headNode;
-	listint_t *tmpMem;
+	listint_t *tmpMem, *emptyList;
 
 	if (*head == NULL)
 	{
@@ -20,7 +20,8 @@ int pop_listint(listint_t **head)
 
 	tmpMem = *head;
 	headNode = tmpMem->n;
+	emptyList= tmpNext->next;
 	free(tmpMem);
-	*head = NULL
+	*head = emptyList;
 	return (headNode);
 }
