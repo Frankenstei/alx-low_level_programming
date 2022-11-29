d6540deec2a1fd500145a4b3ef370f483f43b049
Frankenstei <enwereloveday@yahoo.com>
2022-11-29 12:48:10 -0800

    free and NULL head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list and head NULL
+ *
+ * @head: first node
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listiny_t *tmpMem, *mainMem;
+
+	if (head != NULL)
+	{
+		mainMem = *head;
+
+		while ((tmpMem = mainMem) != NULL)
+		{
+			mainMem = mainMem->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
