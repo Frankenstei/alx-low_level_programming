bd9c66928b4670cf0a50965845a647c9cfa8175f
Frankenstei <enwereloveday@yahoo.com>
2022-11-29 19:45:57 -0800

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * *reverse_listint - reverses a list
+ *
+ * @head: first node
+ *
+ * Return; list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmpMem = NULL, *mainMem = NULL;
+	while (*head != NULL)
+	{
+		mainMem = (*head)->next;
+		(*head)->next = tmpMem;
+		tmpMem = *head;
+		*head = mainMem;
+	}
+	*head = tmpMem;
+	return (*head);
+}
+
