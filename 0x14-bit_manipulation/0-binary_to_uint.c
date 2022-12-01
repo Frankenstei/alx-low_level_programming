0b9a39795bbb1a81eaa69e45c292677e7723afd2
Frankenstei <enwereloveday@yahoo.com>
2022-12-01 12:19:05 -0800

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,19 +12,26 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int uint;
 
-	for (uint = 0; *b; b++)
+	int length, multiplier = 1;
+
+	if (b == NULL)
 	{
-		if (b != '0' || b != '1' || b == NULL)
+		return (0);
+	}
+	for (length; b[length] != '\0'; length++)
+	{
+		;
+	}
+
+	for (length--; length > 0; length--, multiplier = multiplier * 2)
+	{
+		if (b != '0' || b != '1')
 		{
 			return (0);
 		}
-		else if (b == '1')
+		if (b[length] & 1)
 		{
-			uint = (uint << 1) | 1;
-		}
-		else if (b == '0')
-		{
-			uint = uint << 1;
+			uint = uint + multiplier;
 		}
 	}
 	return (uint);
