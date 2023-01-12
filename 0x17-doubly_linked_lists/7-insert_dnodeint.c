e1e039a13b98f1795c808a27c099b50185ab9750
Frankenstei <enwereloveday@yahoo.com>
2023-01-12 12:59:15 -0800

    all

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,10 +11,10 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new
-
-		if (idx == 0)
-			return ( add_dnodeint(h, n));
+	dlistint_t *tmp = *h, *new;
+	
+	if (idx == 0)
+		return ( add_dnodeint(h, n));
 
 	for (; idx != 1; idx--)
 	{
