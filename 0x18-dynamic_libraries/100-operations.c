36016cf4452dcc66bf81434d3175561127b9c0c1
Frankenstei <enwereloveday@yahoo.com>
2023-01-16 06:42:30 -0800

    dlib

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+/**
+ * add - addition
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
