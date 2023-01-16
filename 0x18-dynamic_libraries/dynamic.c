dda7c5f2f9a8025ce6b118b91cd6e5ecf2b1c083
Frankenstei <enwereloveday@yahoo.com>
2023-01-16 06:19:31 -0800

    dlib

---
+++ 0x18-dynamic_libraries/dynamic.c
@@ -0,0 +1,192 @@
+#include "main.h"
+
+/**
+ * _putchar - prints one character
+ * @c: character
+ * Return: character
+ */
+int _putchar(char c)
+{
+}
+
+/**
+ * _islower - checks for lower case
+ * @c: character
+ * Return: 1 or -1
+ */
+int _islower(int c)
+{
+}
+
+/**
+ * _isalpha - checks for uppercase
+ * @c: character
+ * Return: 1 or -1
+ */
+int _isalpha(int c)
+{
+}
+
+/**
+ * _abs - finds absolute value
+ * @n: number
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+}
+
+/**
+ * _isupper - checks for uppercase
+ * @c: character
+ * Return: 1 or -1
+ */
+int _isupper(int c)
+{
+}
+
+/**
+ * _isdigit - checks for digit
+ * @c: digit
+ * Return: 1 or -1
+ */
+int _isdigit(int c)
+{
+}
+
+/**
+ * _strlen - length of string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+}
+/**
+ * _puts - returns character
+ * @s: character
+ * Return: nothing
+ */
+void _puts(char *s)
+{
+}
+
+/**
+ * *_strcpy - copy string
+ * @dest: destination
+ * @src: source
+ * Return: address
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
+
+/**
+ * _atoi - converts character to integer
+ * @s: string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+}
+
+/**
+ * *_strcat - store and print
+ * @dest: destination
+ * @src: source
+ * Return: address
+ */
+char *_strcat(char *dest, char *src)
+{
+}
+
+/**
+ * *_strncat - store n and print
+ * @dest: destination
+ * @src: source
+ * @n: number of characters
+ * Return: address
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+}
+
+/**
+ * *_strncpy - copy n characters
+ * @dest: destination
+ * @src: source
+ * @n: number of characters
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+}
+
+/**
+ * _strcmp - compare
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1, 0, or -1
+ */
+int _strcmp(char *s1, char *s2)
+{
+}
+
+/**
+ * *_memset - set memory
+ * @s: string
+ * @b: bytes
+ * @n: number
+ * Return: address
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
+
+/**
+ * *_memcpy - copy memory
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: address
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
+/**
+ * *_strchr - strchr
+ * @s: string
+ * @c: character
+ * Return: address
+ */
+char *_strchr(char *s, char c)
+{
+}
+/**
+ * _strspn - strspn
+ * @s: string
+ * @accept: accept
+ * Return: integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+}
+
+/**
+ * *_strpbrk - strpbrk
+ * @s: string
+ * @accept: accept
+ * Return: address
+ */
+char *_strpbrk(char *s, char *accept)
+{
+}
+
+/**
+ * *_strstr - strstr
+ * @haystack: haystack
+ * @needle: needle
+ * Return: address
+ */
+char *_strstr(char *haystack, char *needle)
+{
+}
