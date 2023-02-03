9301699799599470f27666f909fe206f2bb21cef
Frankenstei <enwereloveday@yahoo.com>
2023-02-03 14:43:54 -0800

    h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,10 @@
-#ifndef __HASH
-#define __HASH
+#ifndef _HASH
+#define _HASH
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
